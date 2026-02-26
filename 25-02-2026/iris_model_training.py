dd7aa51009158be6483cd0f482660bae55e0ba5c
Yashaswinivs <yashaswinivsgowda@gmail.com>
2026-02-26 21:26:59 +0530

    initial commit

--- 25-02-2026/iris_model_training.py
+++ 25-02-2026/iris_model_training.py
@@ -1,13 +1,7 @@
 # Step 1: Import required libraries
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-from sklearn.ensemble import RandomForestClassifier
+
+
 
 # Step 2: Load dataset
 df = pd.read_csv("iris.csv")
